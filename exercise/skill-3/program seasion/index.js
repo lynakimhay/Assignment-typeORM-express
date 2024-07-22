8a64111ffac78131cc57df471ee0c1db7f015f3b
lynakimhay <nakimhay.ly@institute.pse.ngo>
2024-07-22 15:14:27 +0700

    layout6-layout9

---
+++ exercise/skill-3/program seasion/index.js
@@ -0,0 +1,15 @@
+function doubleChar(str){
+    // Write your code here
+    return ""
+   }
+   // Example "he"
+   // h => hh
+   // e => ee
+   // Result: he => hhee
+   // Test with this strings
+   console.log(doubleChar("String"))
+   // Output: 'SSttrriinngg'
+   console.log(doubleChar("Hello World"))
+   // Output: 'HHeelllloo WWoorrlldd'
+   console.log(doubleChar("1234!_ "))
+   // Output: '11223344!!__ '
